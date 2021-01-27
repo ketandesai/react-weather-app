1127ee14dff8eef133c694ea32382b974d8d922d
Ketan Desai <desai.ketan@gmail.com>
2021-01-27 14:24:20 -0500

    cleaned up files

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
-//import './api/server'
 import App from './App'
 import store from './app/store'
 import { fetchLocation } from './reducers/locationSlice'
