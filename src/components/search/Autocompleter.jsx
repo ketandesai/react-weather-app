275378cec11f1052ba411e72c773e56704f3d1e8
Ketan Desai <desai.ketan@gmail.com>
2021-01-28 10:58:42 -0500

    added license

--- src/components/search/Autocompleter.jsx
+++ src/components/search/Autocompleter.jsx
@@ -1,106 +1,104 @@
-import React, { useState, useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import "./Autocompleter.css";
-import { fetchForward, selectFeatures } from "../../reducers/geocodeSlice";
-import { locationUpdated } from "../../reducers/locationSlice";
-import { fetchWeather } from "../../reducers/weatherSlice";
-import { OPEN_WEATHER_BASE_URL} from "../../api/config.js";
+import React, { useState, useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import './Autocompleter.css'
+import { fetchForward, selectFeatures } from '../../reducers/geocodeSlice'
+import { locationUpdated } from '../../reducers/locationSlice'
+import { fetchWeather } from '../../reducers/weatherSlice'
+import { OPEN_WEATHER_BASE_URL } from '../../api/config.js'
 
-const ENTER_KEY = 13;
-const UP_ARROW_KEY = 38;
-const DOWN_ARROW_KEY = 40;
+const ENTER_KEY = 13
+const UP_ARROW_KEY = 38
+const DOWN_ARROW_KEY = 40
 
 export const Autocompleter = () => {
-  const dispatch = useDispatch();
-  const [activeSuggestion, setActiveSuggestion] = useState(0);
-  const [filteredSuggestions, setFilteredSuggestions] = useState([]);
-  const [showSuggestions, setShowSuggestions] = useState(false);
-  const [userInput, setUserInput] = useState("");
-  const [lat, setLat] = useState("");
-  const [lon, setLon] = useState("");
-  const [locationSelected, setLocationSelected] = useState("");
-  const places = useSelector(selectFeatures);
+  const dispatch = useDispatch()
+  const [activeSuggestion, setActiveSuggestion] = useState(0)
+  const [filteredSuggestions, setFilteredSuggestions] = useState([])
+  const [showSuggestions, setShowSuggestions] = useState(false)
+  const [userInput, setUserInput] = useState('')
+  const [lat, setLat] = useState('')
+  const [lon, setLon] = useState('')
+  const [locationSelected, setLocationSelected] = useState('')
+  const places = useSelector(selectFeatures)
 
   useEffect(() => {
     if (userInput) {
-      dispatch(fetchForward(userInput));
+      dispatch(fetchForward(userInput))
       if (locationSelected && lat && lon) {
-        let allWeatherUrl = `${OPEN_WEATHER_BASE_URL}/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${process.env.REACT_APP_OPEN_WEATHER_API_KEY}&units=imperial`;
-        dispatch(fetchWeather(allWeatherUrl));
+        let allWeatherUrl = `${OPEN_WEATHER_BASE_URL}/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${process.env.REACT_APP_OPEN_WEATHER_API_KEY}&units=imperial`
+        dispatch(fetchWeather(allWeatherUrl))
         dispatch(
           locationUpdated({ lat: lat, lon: lon, city: locationSelected })
-        );
+        )
       }
     }
-  }, [userInput, dispatch, locationSelected, lat, lon]);
+  }, [userInput, dispatch, locationSelected, lat, lon])
 
   const onChange = (e) => {
-    const suggestions = places?.map((data) => data.place_name);
-    setActiveSuggestion(0);
-    setFilteredSuggestions(suggestions);
-    setShowSuggestions(true);
-    setUserInput(e.currentTarget.value);
-    setLocationSelected(null);
-  };
+    const suggestions = places?.map((data) => data.place_name)
+    setActiveSuggestion(0)
+    setFilteredSuggestions(suggestions)
+    setShowSuggestions(true)
+    setUserInput(e.currentTarget.value)
+    setLocationSelected(null)
+  }
 
   const onClick = (e) => {
     const placeSelected = places.find(
       (element) => element.place_name === e.currentTarget.innerText
-    );
-    setLon(placeSelected?.center[0]);
-    setLat(placeSelected?.center[1]);
-    setLocationSelected(e.currentTarget.innerText);
-    setActiveSuggestion(0);
-    setFilteredSuggestions([]);
-    setShowSuggestions(false);
-    setUserInput(e.currentTarget.innerText);
-  };
+    )
+    setLon(placeSelected?.center[0])
+    setLat(placeSelected?.center[1])
+    setLocationSelected(e.currentTarget.innerText)
+    setActiveSuggestion(0)
+    setFilteredSuggestions([])
+    setShowSuggestions(false)
+    setUserInput(e.currentTarget.innerText)
+  }
 
   const onKeyDown = (e) => {
-    console.log("onKeyDown");
-
     if (e.keyCode === ENTER_KEY) {
-      setActiveSuggestion(0);
-      setShowSuggestions(false);
-      setUserInput(filteredSuggestions[activeSuggestion]);
+      setActiveSuggestion(0)
+      setShowSuggestions(false)
+      setUserInput(filteredSuggestions[activeSuggestion])
     } else if (e.keyCode === UP_ARROW_KEY) {
       if (activeSuggestion === 0) {
-        return;
+        return
       }
-      setActiveSuggestion(activeSuggestion - 1);
+      setActiveSuggestion(activeSuggestion - 1)
     } else if (e.keyCode === DOWN_ARROW_KEY) {
       if (activeSuggestion - 1 === filteredSuggestions.length) {
-        return;
+        return
       }
     }
-    setActiveSuggestion(activeSuggestion + 1);
-  };
+    setActiveSuggestion(activeSuggestion + 1)
+  }
 
-  let suggestionsListComponent;
+  let suggestionsListComponent
   if (showSuggestions && userInput) {
     if (places?.length > 0) {
       suggestionsListComponent = (
         <ul className="suggestions">
           {places.map((suggestion, index) => {
-            let className;
+            let className
 
             if (index === activeSuggestion) {
-              className = "suggestion-active";
+              className = 'suggestion-active'
             }
             return (
               <li className={className} key={index} onClick={onClick}>
                 {suggestion.place_name}
               </li>
-            );
+            )
           })}
         </ul>
-      );
+      )
     } else {
       suggestionsListComponent = (
         <div className="flex justify-center no-suggestions">
           <em>No suggestions available.</em>
         </div>
-      );
+      )
     }
   }
   return (
@@ -108,8 +106,8 @@ export const Autocompleter = () => {
       <div className="space-y-0">
         <input
           style={{
-            backgroundColor: "#e2e8f0",
-            letterSpacing: "0.05em"
+            backgroundColor: '#e2e8f0',
+            letterSpacing: '0.05em',
           }}
           className={`data-hj-whitelist block appearance-none w-full border-none rounded-full shadow py-3 pl-12 pr-6 mb-0 leading-tight focus:outline-none focus:bg-gray-200 truncate`}
           type="text"
@@ -121,5 +119,5 @@ export const Autocompleter = () => {
         {suggestionsListComponent}
       </div>
     </>
-  );
-};
+  )
+}
--- src/components/weather/WeatherDetail.jsx
+++ src/components/weather/WeatherDetail.jsx
@@ -1,11 +1,12 @@
-import React from "react";
-import { TimeDateComponent } from "./TimeDateComponent";
-import { Temperature } from "./Temperature";
-import { MinutelyForecast } from "./MinutelyForecast";
+import React from 'react'
+import { TimeDateComponent } from './TimeDateComponent'
+import { Temperature } from './Temperature'
+import { MinutelyForecast } from './MinutelyForecast'
+import Toggle from 'react-toggle'
 
 export const WeatherDetail = ({ weather, location, units }) => {
-  let region = location?.region ? `, ${location.region}` : "";
-  let country = location?.country ? `, ${location.country}` : "";
+  let region = location?.region ? `, ${location.region}` : ''
+  let country = location?.country ? `, ${location.country}` : ''
   return (
     <>
       <div>
@@ -20,6 +21,9 @@ export const WeatherDetail = ({ weather, location, units }) => {
           showDate={true}
           showTime={true}
         />
+      </div>
+      <div>
+        <Toggle />
       </div>
       <div className="sm:flex-col md:flex md:flex-row justify-between my-2 px-6 sm:mt-5 sm:mb-5 sm:px-4">
         <div className="flex-col sm:w-full lg:w-1/2">
@@ -36,7 +40,7 @@ export const WeatherDetail = ({ weather, location, units }) => {
 
             <div>
               <span className="text-6xl font-bold">
-                <Temperature degrees={weather?.temp} units={units} />{" "}
+                <Temperature degrees={weather?.temp} units={units} />{' '}
               </span>
             </div>
           </div>
@@ -44,16 +48,17 @@ export const WeatherDetail = ({ weather, location, units }) => {
 
         <div className="flex flex-col justify-center items-center sm:mt-6 sm:w-full lg:w-1/2">
           <div className="text-sm sm:text-lg ml-8 mt-3 sm:mt-0">
-            Feels like:{" "}
+            Feels like:{' '}
             <span className="font-bold">
               <Temperature degrees={weather?.feels_like} units={units} />
             </span>
             <br />
-            <span><MinutelyForecast /></span>
-            
+            <span>
+              <MinutelyForecast />
+            </span>
           </div>
         </div>
       </div>
     </>
-  );
-};
+  )
+}
