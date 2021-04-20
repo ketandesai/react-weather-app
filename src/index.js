82615830e1f4ea05eab11a605d2c196c573cfa56
ketandesai <desai.ketan@gmail.com>
2021-04-20 16:26:38 -0400

    added pwa support

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ import App from './App'
 import store from './app/store'
 import { fetchLocation } from './reducers/locationSlice'
 
+import * as serviceWorkerRegistration from './serviceWorkerRegistration'
+import reportWebVitals from './reportWebVitals'
+
 store.dispatch(fetchLocation())
 
 ReactDOM.render(
@@ -15,3 +18,13 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 )
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register()
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals()
